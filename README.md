7b7148eb895f3ede44e14c20c058b1c8be4a779c
Chris Zuber <shgysk8zer0@gmail.com>
2017-07-20 20:03:15 -0700

    Add initial set of resources and submodules

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# about
+# about [![Build Status](https://travis-ci.org/shgysk8zer0/about.svg?branch=master)](https://travis-ci.org/shgysk8zer0/about)
 Project for creating simple about pages.
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    entry: './js/index.js',
+    output: {
+        path: __dirname,
+        filename: 'js/index.min.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['latest'],
+                    plugins: ['transform-runtime']
+                }
+            }
+        ]
+    },
+    plugins: [
+        // Avoid publishing files when compilation fails
+        new webpack.NoErrorsPlugin(),
+		new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}})
+    ],
+    stats: {
+        // Nice colored output
+        colors: true
+    },
+    // Create Sourcemaps for the bundle
+    devtool: 'source-map'
+};
