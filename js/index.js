900c6efd8d88161ca4f20580b3b923a755c1886a
Chris Zuber <shgysk8zer0@gmail.com>
2017-09-19 16:44:54 -0700

    Use portfolio.json for portfolio sections - Create `<template>` for projects - Remove existing sections - Load projects from `portfolio.json` - Populate & append templates

--- js/index.js
+++ js/index.js
@@ -4,6 +4,23 @@ import * as Mutations from './mutations.js';
 $(self).ready(async () => {
 	Mutations.init();
 	$(document.body).watch(Mutations.events, Mutations.options, Mutations.filter);
+	const resp = await fetch(new URL('portfolio.json', location.href));
+	if (resp.ok) {
+		const main = document.querySelector('main');
+		const template = document.getElementById('project-template');
+		const projects = await resp.json();
+		projects.forEach(project => {
+			const card = template.content.cloneNode(true);
+			card.querySelector('[itemprop="name"]').textContent = project.name;
+			card.querySelector('[itemprop="image"]').src = project.image;
+			card.querySelector('[itemprop="description"]').textContent = project.description;
+			card.querySelector('[itemprop="url"]').href = project.url;
+			card.querySelector('[itemtype]').setAttribute('itemscope', '');
+			main.appendChild(card);
+		});
+	} else {
+		throw new Error(`${resp.url} [${resp.status} ${resp.statusText}]`);
+	}
 
 }, {once: true});
 $(self).load(() => $('main').intersect((entries, observer) => {
