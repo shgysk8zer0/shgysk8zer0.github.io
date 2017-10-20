82f310f7f4cdf120c7d726542fb699a57b512f91
Chris Zuber <shgysk8zer0@gmail.com>
2017-10-19 17:38:29 -0700

    Rename share.text -> share.label

--- js/share-config.js
+++ js/share-config.js
@@ -1,29 +1,29 @@
 export const facebook = {
 	url: new URL('https://www.facebook.com/sharer/sharer.php?u&t'),
 	icon: new URL('/img/logos/Facebook.svg', location.origin),
-	text: 'Facebook',
+	label: 'Facebook',
 };
 
 export const twitter = {
 	url: new URL('https://twitter.com/intent/tweet/?text&url'),
 	icon: new URL('/img/logos/twitter.svg', location.origin),
-	text: 'Twitter',
+	label: 'Twitter',
 };
 
 export const googlePlus = {
 	url: new URL('https://plus.google.com/share/?url'),
 	icon: new URL('/img/logos/Google_plus.svg', location.origin),
-	text: 'Google+',
+	label: 'Google+',
 };
 
 export const linkedIn = {
 	url: new URL('https://www.linkedin.com/shareArticle/?title&summary&url'),
 	icon: new URL('/img/logos/linkedin.svg', location.origin),
-	text: 'LinkedIn',
+	label: 'LinkedIn',
 };
 
 export const reddit = {
 	url: new URL('https://www.reddit.com/submit/?url&title'),
 	icon: new URL('/img/logos/Reddit.svg', location.origin),
-	text: 'Reddit',
+	label: 'Reddit',
 };
