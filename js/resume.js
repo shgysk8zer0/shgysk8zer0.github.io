01a9badfb5912de00cab290fafab931cf7bc8dad
Chris Zuber <shgysk8zer0@gmail.com>
2018-01-24 18:58:42 -0800

    Improve consistency between resume and site
    - Copy share button style to site
    - Use consistent `data-click="print"` attribute on resume
    - Make resume JS ES5 for better compatibility

--- js/resume.js
+++ js/resume.js
@@ -1,15 +1,19 @@
-(() => {
+(function() {
 	function ready() {
-		return new Promise(resolve => {
+		return new Promise(function(resolve) {
 			if (document.readyState === 'interactive') {
 				resolve();
 			} else {
-				document.addEventListener('DOMContentLoaded', () => resolve(), {once: true});
+				document.addEventListener('DOMContentLoaded', function() {
+					resolve();
+				}, {once: true});
 			}
 		});
 	}
 
-	function $(selector, base = document) {
+
+	function $(selector, base) {
+		base = base || document;
 		const items = Array.from(base.querySelectorAll(selector));
 		if (base instanceof HTMLElement && base.matches(selector)) {
 			items.unshift(base);
@@ -24,31 +28,32 @@
 		});
 	}
 
-	ready().then(() => {
+	return ready().then(function() {
 		if (document.body.contextMenu instanceof HTMLMenuElement) {
-			$('section').forEach(section => {
-				const heading = section.querySelector('.section-heading');
+			$('section').forEach(function(section) {
+				var heading = section.querySelector('.section-heading');
 				if (heading instanceof HTMLElement) {
-					const menuitem = document.createElement('menuitem');
+					var menuitem = document.createElement('menuitem');
 					menuitem.setAttribute('label', heading.textContent);
 					menuitem.dataset.scrollTo = section.id;
-					menuitem.addEventListener('click', event => scrollTo(event.target.dataset.scrollTo));
+					menuitem.addEventListener('click', function(event) {
+						scrollTo(event.target.dataset.scrollTo);
+					});
 					document.body.contextMenu.append(menuitem);
 				}
 			});
 		}
 
-		if (window.print instanceof Function) {
-			$('[data-action="print"]').forEach(btn => {
-				btn.addEventListener('click', () => print());
-				btn.hidden = false;
+		$('[data-click="print"]').forEach(function(btn) {
+			btn.addEventListener('click', function() {
+				print();
 			});
-		}
+		});
 
 		if (Navigator.prototype.hasOwnProperty('share')) {
-			$('[data-share]').forEach(share => {
+			$('[data-share]').forEach(function(share) {
 				share.removeAttribute('hidden');
-				share.addEventListener('click', () => {
+				share.addEventListener('click', function() {
 					navigator.share({
 						url: location.href,
 						title: document.title,
@@ -57,5 +62,5 @@
 				});
 			});
 		}
-	}).catch(console.error);
-})();
+	});
+})().catch(console.error);
